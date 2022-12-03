4f0cfbab33693f59837b686c3d5535fe0a38ed0c
Tobias Bladh <tobias.bladh@live.se>
2022-12-03 16:42:11 +0100

    Add completed day 3 part 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,10 +3,10 @@ import year2021._
 
 @main def entry = {
     
-    val path = "inputs/2021/day2/part1.txt"
+    val path = "inputs/2021/day3/part1.txt"
     val input = Source.fromFile(path).mkString
     
     println(s"last entry check: ${input.lines().toArray().last}")
 
-    println(day2.part2(input))
+    println(day3.part1(input))
 }
